32e1314a01de3c169bf686b8e702d9153a3fd77a
nithin kumar <nithinkottary25@gmail.com>
2026-09-08 15:01:00 +0530

    chapter-0 finished with assignments

---
+++ chapter-00/assignments.py
@@ -0,0 +1,13 @@
+# Assignments
+
+# print hello world
+print("Hello World")
+
+# print hello world 10 times
+
+# method 1
+print("Hello World\n" * 10)
+
+# method 2
+for i in range(10):
+    print("Hello World")
---
+++ chapter-00/intro_notes.md
@@ -0,0 +1,11 @@
+# What python is
+
+- 1. easy to learn
+- 2. its english only
+- 3. easy to learn and adopt fast.
+
+# Why python
+
+- 1. AI/ML field
+- 2. Web dev
+- 3. Data analytics
